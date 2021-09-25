76a3530f659bef20e92cfce0131ad75c443ce063
Andus <30293380+AndusDEV@users.noreply.github.com>
2021-09-25 22:54:16 +0200

    Even More Themes Work

--- src/main/java/pl/andus/skeditor/Themes.java
+++ src/main/java/pl/andus/skeditor/Themes.java
@@ -1,10 +1,11 @@
 package pl.andus.skeditor;
 
-import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.Token;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 import static pl.andus.skeditor.Main.*;
 
@@ -49,7 +50,6 @@ public class Themes {
             menuBar.setBackground(Color.darkGray);
             // textarea
             textArea.setCurrentLineHighlightColor(Color.gray);
-            textArea.setBackground(Color.darkGray);
             textArea.setForeground(Color.white);
             //text
             scheme.getStyle(Token.RESERVED_WORD).foreground = new Color(53, 154, 255);
@@ -63,7 +63,6 @@ public class Themes {
             menuBar.setBackground(Color.white);
             // textarea
             textArea.setCurrentLineHighlightColor(new Color(255, 236, 121));
-            textArea.setBackground(Color.white);
             textArea.setForeground(Color.black);
             // text
             scheme.getStyle(Token.RESERVED_WORD).foreground = Color.blue;
@@ -73,5 +72,20 @@ public class Themes {
             scheme.getStyle(Token.OPERATOR).foreground = new Color(178, 51, 197);
             scheme.getStyle(Token.LITERAL_NUMBER_DECIMAL_INT).foreground = Color.lightGray;
         }
+    }
+
+    public static void Metalic() {
+        // menu
+        menuBar.setBackground(Color.gray);
+        // textarea
+        textArea.setCurrentLineHighlightColor(new Color(255, 236, 121));
+        textArea.setForeground(Color.black);
+        // text
+        scheme.getStyle(Token.RESERVED_WORD).foreground = Color.blue;
+        scheme.getStyle(Token.RESERVED_WORD_2).foreground = Color.orange;
+        scheme.getStyle(Token.COMMENT_KEYWORD).foreground = Color.magenta;
+        scheme.getStyle(Token.DATA_TYPE).foreground = new Color(53, 154, 255);
+        scheme.getStyle(Token.OPERATOR).foreground = new Color(178, 51, 197);
+        scheme.getStyle(Token.LITERAL_NUMBER_DECIMAL_INT).foreground = Color.lightGray;
     }
 }
